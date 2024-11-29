897afa69a609a9742022fbbc7b8ff40b7827e89e
Felipe <felipeclave@gmail.com>
2024-11-29 14:59:05 -0300

    página de favoritos e função de favoritar utilizando context e hooks

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,8 +1,22 @@
-const { default: Cabecalho } = require("components/Cabecalho");
+import Banner from "components/Banner";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
+import videos from "json/db.json";
+import styles from "./Inicio.module.css";
 
 function Inicio() {
-    return(
-        <Cabecalho />
+    return (
+        <>
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <section className={styles.container}> 
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id}/>
+                })}
+            </section>
+        </>
     )
 }
 
